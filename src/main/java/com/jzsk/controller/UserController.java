e688e4e9dd5477db009597be3b49d54e2667d4ce
EdisonM79 <mopengxi@163.com>
2019-01-07 14:06:33 +0800

    edit log properties

--- src/main/java/com/jzsk/controller/UserController.java
+++ src/main/java/com/jzsk/controller/UserController.java
@@ -3,6 +3,7 @@ package com.jzsk.controller;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
